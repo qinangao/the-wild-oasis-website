9841b348fc7e5714eee45ee875da9b5e082cdeb4
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-03 13:21:17 +0100

    add home page

--- app/page.js
+++ app/page.js
@@ -5,7 +5,14 @@ import bg from "@/public/bg.png";
 export default function Page() {
   return (
     <main className="mt-24">
-      <Image src={bg} fill alt="Mountains and forests with two cabins" />
+      <Image
+        src={bg}
+        fill
+        quality={80}
+        placeholder="blur"
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
 
       <div className="relative z-10 text-center">
         <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
