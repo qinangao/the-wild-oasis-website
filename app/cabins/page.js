ea0095ba51586f6b5177de8722b59a6d771ceaf3
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-03 11:39:56 +0100

    build layout, add matadata

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,10 @@
+export const metadata = {
+  title: "Cabins",
+};
 export default function Page() {
-  return <h1>Cabins page</h1>;
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
 }
