734e354b7a6d7336f0f5b20a8a79207cb8951156
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-03 14:50:56 +0100

    refactor: update CabinCard to use Next.js Image and Link components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,12 @@
 import CabinCard from "../_components/CabinCard";
+import { getCabin, getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
-export default function Page() {
-  // CHANGE
-  const cabins = [];
+export default async function Page() {
+  const cabins = await getCabins();
+  console.log(cabins);
 
   return (
     <div>
