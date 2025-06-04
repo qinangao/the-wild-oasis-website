0daf44a50c3d93a2d1c9d71bd4c4cb8ba5b55c17
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-04 13:29:51 +0100

    feat: implement CabinList component and integrate with cabin Page, use suspense for cabin data loading

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,11 @@
-import CabinCard from "../_components/CabinCard";
-import { getCabin, getCabins } from "../_lib/data-service";
+import CabinList from "@/app/_components/CabinList";
+import { Suspense } from "react";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
-export default async function Page() {
-  const cabins = await getCabins();
-  console.log(cabins);
-
+export default function Page() {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,13 +20,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
