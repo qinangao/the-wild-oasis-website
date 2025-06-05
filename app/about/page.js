248a4b9df65679f8faec1fff74e7932bf8c779ee
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-05 15:00:43 +0100

    fetching number of cabins

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import image1 from "@/public/about-1.jpg";
 import Link from "next/link";
+import { getCabins } from "../_lib/data-service";
 // import image2 from "@/public/about-2.jpg";
 
 export const metadata = {
   title: "About",
 };
-export default function Page() {
+export const revalidate = 86400;
+
+export default async function Page() {
+  const cabins = await getCabins();
+  const numCabins = cabins.length;
+
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
       <div className="col-span-3">
@@ -23,10 +29,10 @@ export default function Page() {
             and enjoying simple pleasures with family.
           </p>
           <p>
-            Our 8 luxury cabins provide a cozy base, but the real freedom and
-            peace you&apos;ll find in the surrounding mountains. Wander through
-            lush forests, breathe in the fresh air, and watch the stars twinkle
-            above from the warmth of a campfire or your hot tub.
+            Our {numCabins} luxury cabins provide a cozy base, but the real
+            freedom and peace you&apos;ll find in the surrounding mountains.
+            Wander through lush forests, breathe in the fresh air, and watch the
+            stars twinkle above from the warmth of a campfire or your hot tub.
           </p>
           <p>
             This is where memorable moments are made, surrounded by
@@ -51,6 +57,7 @@ export default function Page() {
           fill
           className="object-cover"
           alt="Family that manages The Wild Oasis"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
 
