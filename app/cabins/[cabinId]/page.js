042f2ccd367103b1f6953d8648deb647dd0b7a19
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-05 15:50:58 +0100

    implement text expender in cabinId page(CC in SC)

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,3 +1,4 @@
+import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 
@@ -12,9 +13,8 @@ export async function generateMetadata({ params }) {
 }
 export async function generateStaticParams(params) {
   const cabins = await getCabins();
-  //   console.log(cabins);
   const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
-  //   console.log(ids);
+
   return ids;
 }
 
@@ -42,7 +42,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
