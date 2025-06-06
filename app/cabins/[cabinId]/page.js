52917d6066f06a8c45097c8f6795f4c24b53066f
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-06 15:30:59 +0100

    add dateselector and reservation form components to cabinId page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,3 +1,5 @@
+import DateSelector from "@/app/_components/DateSelector";
+import ReservationForm from "@/app/_components/ReservationForm";
 import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
@@ -72,9 +74,14 @@ export default async function Page({ params }) {
       </div>
 
       <div>
-        <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
+        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+          Reserve {name} today. Pay on arrival.
         </h2>
+
+        <div className="grid grid-cols-2 gap-3 border border-primary-800 min-h-[400px]">
+          <DateSelector />
+          <ReservationForm />
+        </div>
       </div>
     </div>
   );
