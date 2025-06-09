21211c0cf7f92d278cc6df75def4b7a30212a5be
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-09 15:59:38 +0100

    add authentication setup with NextAuth and Google provider

---
+++ app/_lib/auth.js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   providers: [Google],
+// });
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+};
+
+export const {
+  auth,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
