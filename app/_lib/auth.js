0754cf38f3cabd83004d2b51263fd845449bc40f
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-09 20:15:19 +0100

    feat: implement guest creation and session handling in authentication

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 // export const { handlers, auth, signIn, signOut } = NextAuth({
 //   providers: [Google],
@@ -15,6 +16,22 @@ const authConfig = {
   callbacks: {
     authorized({ auth }) {
       return !!auth?.user;
+    },
+    async signIn({ user }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
