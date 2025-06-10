c013a2e73967ca48d118b4452d4905c428babdee
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-10 14:12:00 +0100

    feat: add key prop to SelectCountry component for improved rendering

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -25,6 +25,7 @@ export default async function Page() {
         <SelectCountry
           name="nationality"
           id="nationality"
+          key={guest.nationality}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           defaultCountry={guest.nationality}
         />
