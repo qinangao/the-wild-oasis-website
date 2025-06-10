42a85eba19c23c8b1c5e41b7d9aa5d4bdb4ca4a6
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-10 15:27:01 +0100

    feat: implement delete reservation functionality with authorization checks

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -33,4 +34,25 @@ export async function updateGuestProfile(formData) {
     throw new Error("Guest could not be updated");
   }
   revalidatePath("/account/profile");
+}
+
+export async function deleteRevervation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in.");
+  const guestBooking = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBooking.map((booking) => booking.id);
+  
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be deleted");
+  }
+  revalidatePath("account/reservations");
 }
