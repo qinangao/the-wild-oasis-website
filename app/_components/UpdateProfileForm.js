79b207e172b0a767435fa808ff8f5da3c223b414
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-10 13:51:10 +0100

    feat: enhance profile update functionality with guest data and form handling

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,18 +1,21 @@
 "use client";
-import { useState } from "react";
 
-function UpdateProfileForm({ children }) {
-  // // CHANGE
-  const countryFlag = "/pt.jpg";
-  const nationality = "portugal";
+import { updateGuestProfile } from "../_lib/actions";
 
-  const [count, setCount] = useState();
+function UpdateProfileForm({ guest, children }) {
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
+
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+      action={updateGuestProfile}
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name="fullName"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -21,6 +24,8 @@ function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          name="email"
+          defaultValue={email}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -30,6 +35,7 @@ function UpdateProfileForm({ children }) {
           <label htmlFor="nationality">Where are you from?</label>
           <img
             src={countryFlag}
+            defaultValue={nationality}
             alt="Country flag"
             className="h-5 rounded-sm object-cover"
           />
@@ -41,6 +47,7 @@ function UpdateProfileForm({ children }) {
         <label htmlFor="nationalID">National ID number</label>
         <input
           name="nationalID"
+          defaultValue={nationalID}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,7 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -8,4 +9,27 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateGuestProfile(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in.");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid nationalID number");
+
+  const updateData = { nationalID, countryFlag, nationality };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be updated");
+  }
+  return data;
 }
