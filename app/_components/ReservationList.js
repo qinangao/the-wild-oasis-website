9fcbdbfb4dcc1e257efeba1ca2ee5b1810264da8
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-11 19:49:09 +0100

    feat: add ReservationList component with optimistic delete functionality

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,29 @@
+"use client";
+import { useOptimistic } from "react";
+import ReservationCard from "./ReservationCard";
+import { deleteRevervation } from "../_lib/actions";
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteRevervation(bookingId);
+  }
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+          onDelete={handleDelete}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
